927134a8c8e9990f14159af631bcb104a3e295c2
Bryon Meinka <bryon.meinka@gmail.com>
2019-09-18 11:58:45 -0400

    complete hello triangle exercise 3

--- src/hello_triangle_exercise_3.c
+++ src/hello_triangle_exercise_3.c
@@ -2,19 +2,22 @@
 
 #define VERT_PATH "shaders/hello_triangle.vert"
 #define FRAG_PATH "shaders/hello_triangle.frag"
+#define FRAG_ALT_PATH "shaders/hello_triangle_alt.frag"
 
 int main()
 {
 	struct game g;
 
 	/*
-	 * draw two triangles using two different VAOs and VBOs for their data
+	 * create two shader programs where the second program uses a different
+	 * fragment shader that outputs the color yellow; draw two triangles
+	 * where one outputs the color yellow
 	 */
 	float vertices[2][9] = {
 		{-1.0, -0.5, 0.0, 0.0, -0.5, 0.0, -0.5, 0.5, 0.0},
 		{0.0, -0.5, 0.0, 0.5, 0.5, 0.0, 1.0, -0.5, 0.0}
 	};
-	GLuint shader, vaos[2], vbos[2];
+	GLuint shaders[2], vaos[2], vbos[2];
 
 	game_init(&g);
 
@@ -28,8 +31,9 @@ int main()
 		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
 		glEnableVertexAttribArray(0);
 	}
-
-	shader = load_shader(VERT_PATH, FRAG_PATH);
+	
+	shaders[0] = load_shader(VERT_PATH, FRAG_PATH);
+	shaders[1] = load_shader(VERT_PATH, FRAG_ALT_PATH);
 
 	while (!g.quit) {
 		handle_events(&g);
@@ -37,9 +41,8 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shader);
-
 		for (size_t i = 0; i < 2; i++) {
+			glUseProgram(shaders[i]);
 			glBindVertexArray(vaos[i]);
 			glDrawArrays(GL_TRIANGLES, 0, 3);
 			glBindVertexArray(0);
