3c1b9dedf1a3dac68503246264cb42110b307439
Bryon Meinka <bryon.meinka@gmail.com>
2019-09-20 12:16:34 -0400

    shader uniforms

--- src/main.c
+++ src/main.c
@@ -5,7 +5,7 @@
 #endif
 
 #ifndef FRAG_PATH
-#define FRAG_PATH "shaders/hello_triangle.frag"
+#define FRAG_PATH "shaders/uniforms.frag"
 #endif
 
 int main()
@@ -18,6 +18,9 @@ int main()
 		 0.0,  0.5, 0.0
 	};
 	GLuint vao, vbo, shader;
+
+	float green;
+	GLint vertex_color_location;
 
 	game_init(&g);
 
@@ -37,10 +40,15 @@ int main()
 	while (!g.quit) {
 		handle_events(&g);
 
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		green = (sin(get_time()) / 2.0) + 0.5;
+		vertex_color_location = glGetUniformLocation(shader, "our_color");
 		glUseProgram(shader);
+		glUniform4f(vertex_color_location, 0.0f, green, 0.0f, 1.0f);
+
 		glBindVertexArray(vao);
 
 		glDrawArrays(GL_TRIANGLES, 0, 3);
