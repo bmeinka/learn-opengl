fc808a356c753540ddcf3b189c06d569abf675bf
Bryon Meinka <bryon.meinka@gmail.com>
2019-09-20 12:02:56 -0400

    hello triangle

--- src/main.c
+++ src/main.c
@@ -1,15 +1,51 @@
 #include "game.h"
 
+#ifndef VERT_PATH
+#define VERT_PATH "shaders/hello_triangle.vert"
+#endif
+
+#ifndef FRAG_PATH
+#define FRAG_PATH "shaders/hello_triangle.frag"
+#endif
+
 int main()
 {
 	struct game g;
+
+	float vertices[] = {
+		-0.5, -0.5, 0.0,
+		 0.5, -0.5, 0.0,
+		 0.0,  0.5, 0.0
+	};
+	GLuint vao, vbo, shader;
+
 	game_init(&g);
+
+	glGenVertexArrays(1, &vao);
+
+	glBindVertexArray(vao);
+
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0);
+
+	shader = load_shader(VERT_PATH, FRAG_PATH);
 
 	while (!g.quit) {
 		handle_events(&g);
 
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+
+		glUseProgram(shader);
+		glBindVertexArray(vao);
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		glBindVertexArray(0);
 
 		game_swap_window(&g);
 	}
